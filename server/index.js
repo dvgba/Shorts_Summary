31945e106193d7615a84399adf2356fc9e607d5e
dvgba <diegovianagomes01@gmail.com>
2023-09-14 06:27:57 -0300

    Criação do Backend

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import cors from 'cors'
+import express from 'express'
+
+import { download } from './download.js'
+
+const app = express()
+app.use(cors())
+
+app.get('/summary/:id', (request, response) => {
+  download(request.params.id)
+  response.json({result: "Download do video realizado com sucesso!"})
+  //response.send("ID do vídeo: " + request.params.id)
+});
+
+app.listen(3333, () => console.log('Server is running on port 3333'))
